66944596c9580a7ccda64579b34c2105cde43920
Kinny Boy Jolly Knuckles <Bezzykent@gmail.com>
2015-06-18 21:07:44 -0700

    turnwar unity 5 project

---
+++ Assets/Grab.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grab : MonoBehaviour {
+
+	public GameObject mDirectionPointer;
+	public GameObject mControlGrab;
+	public GameObject mCurveLine;
+	public GameObject mShip;
+
+
+	public Vector3 mStartPoint;
+	private Vector3 mTargetPoint;
+	private Vector3 mDirection;
+
+	private Vector3 resultDirectionPoint;
+	private Vector3 resultTargetPoint;
+
+	private Vector3 mLastMousePos;
+
+	private bool mGo;
+	private bool mHasHit;
+
+	public int mSegmentCount;
+
+	void Start () {
+		mHasHit= false;
+		mGo = false;
+
+		mStartPoint = new Vector3(0,4,0);
+		mDirectionPointer.transform.position = new Vector3(0,4,1);
+
+		mShip.transform.position = mStartPoint;
+
+		mDirection = new Vector3(0,0,1.0f);
+	}
+
+	void GoShipGo(){
+		if(mGo == false)
+			mGo = true;
+	}
+
+
+	void Update () 
+	{
+		RaycastHit rayHit;
+
+		if(Input.GetMouseButtonDown(0)) 
+		{
+			//mLastMousePos = Input.mousePosition;
+			//mHasHit = true;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast(ray, out rayHit))
+			{
+				if(rayHit.transform.gameObject.layer == (int)8)
+					mHasHit = true;
+
+				mLastMousePos = Input.mousePosition;
+			}
+		}
+
+
+
+		if(mHasHit)
+		{
+			//Deal with mouse position change
+			Vector2 moveDelta = mLastMousePos - Input.mousePosition;
+			Vector3 pos = new Vector3(moveDelta.x/25.0f, 0, moveDelta.y/25.0f);
+
+			//Deal with camera movement
+			Quaternion camRot = Camera.main.transform.rotation;
+			pos = camRot * pos;
+			pos.y = 0;
+
+			//update control point position
+			mControlGrab.transform.position -= pos;
+			mTargetPoint = mControlGrab.transform.position;
+
+			//Draw the curve
+			for(int i = 0; i <= mSegmentCount; i++)
+			{
+				float t = i / (float) mSegmentCount;
+				Vector3 dir = mTargetPoint - mStartPoint;
+				Vector3 q1 = mStartPoint + ((mDirection + (dir * t)) * t);
+				mCurveLine.GetComponent<LineRenderer>().SetPosition(i, q1);
+
+				if(i == mSegmentCount-1)
+					resultDirectionPoint = q1;
+
+				resultTargetPoint = q1;
+			}
+
+			//find direction of the end points of curve
+			Vector3 d = resultDirectionPoint - resultTargetPoint;
+			float angle = Mathf.Atan2(d.x, d.z) * Mathf.Rad2Deg;
+
+			//rotate the direction pointer
+			Quaternion targetRot = Quaternion.Euler(0,angle - 180.0f,0);
+			mDirectionPointer.transform.rotation = targetRot;
+			mDirectionPointer.transform.position = resultTargetPoint;
+
+			//debug line draw 
+			Debug.DrawLine(mStartPoint, mTargetPoint);
+
+			//mouse pos for next frame
+			mLastMousePos = Input.mousePosition;
+		}
+
+
+		if(Input.GetMouseButtonUp(0) && mHasHit)
+		{
+			mHasHit = false;
+		}
+
+		if(mGo)
+		{
+			//stop updating curve draw 
+			mGo = false;
+
+			//set ship to new rotation/position
+			mShip.transform.position = resultTargetPoint;
+			mShip.transform.rotation = mDirectionPointer.transform.rotation;
+
+			//update direction
+			mDirection = resultTargetPoint - resultDirectionPoint;
+
+			//add magnitude to direction
+			Vector3 dMag = resultTargetPoint - mStartPoint;
+			mDirection = mDirection.normalized * dMag.magnitude;
+
+			//update start point
+			mStartPoint = resultTargetPoint;
+
+			//new target point (start + direction*magnitude)
+			mTargetPoint = resultTargetPoint + mDirection;
+
+			//update ui positions
+			mControlGrab.transform.position = resultTargetPoint;
+			mDirectionPointer.transform.position = mTargetPoint;
+
+			//draw new curve showing next turn direction
+			for(int i = 0; i <= mSegmentCount; i++)
+			{
+				float t = i / (float) mSegmentCount;
+				Vector3 dir = mTargetPoint - mStartPoint;
+				Vector3 q1 = mStartPoint + mDirection * t;
+				mCurveLine.GetComponent<LineRenderer>().SetPosition(i, q1);
+			} 
+		}
+	}
+}
